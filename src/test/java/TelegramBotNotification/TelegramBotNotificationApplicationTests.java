8e88d571ac8319f3090a60ee5567f3d1b6587d51
khilola.kushbakova <xilola_kushbakova@mai.ru>
2024-03-01 10:20:21 +0100

    Initial commit

---
+++ src/test/java/TelegramBotNotification/TelegramBotNotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package TelegramBotNotification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramBotNotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
